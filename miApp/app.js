994a98768d6c51a4f33403f15f73741d60c8cce4
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-11-24 18:18:40 +0100

    acceso a pagina restringida

--- miApp/app.js
+++ miApp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registrarseRouter = require('./routes/registrarse');
+var restringidaRouter = require('./routes/restringida');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/registrarse',registrarseRouter);
-
+app.use('/restringida',restringidaRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
